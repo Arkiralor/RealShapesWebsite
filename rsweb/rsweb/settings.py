ef4628463bf2543c1f744b9fa8cb59a2ad41df99
Arkiralor <pritho11335@gmail.com>
2022-01-07 11:28:04 +0530

    added content.

--- rsweb/rsweb/settings.py
+++ rsweb/rsweb/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w(n(lt9uf75+jesrtxjh@hgw--h+s5ssa---x73i50sul0l9pf'
+SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,23 +79,23 @@ WSGI_APPLICATION = 'rsweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'realshapes',
-#        'USER': 'postgres',
-#        'PASSWORD': 'Driftking11235',
-#        'HOST': 'localhost',
-#        'PORT': '5432'
-#    }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': environ.get('PGDATABASE'),
+       'USER': environ.get('PGUSER'),
+       'PASSWORD': environ.get('PGPASSWORD'),
+       'HOST': environ.get('PGHOST'),
+       'PORT': environ.get('PGPORT')
+   }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
